2a34cca40929e33dfabe5778662fee9371314b7a
Phawit Pornwattanakul <me@helloyeew.dev>
2022-10-01 03:12:59 +0700

    Make the page that's not required to login support guest visit

--- apps/views.py
+++ apps/views.py
@@ -7,21 +7,28 @@ from apps.models import Area, Candidate
 from users.models import ColourSettings
 
 
-@login_required
 def homepage(request):
-    colour_settings = ColourSettings.objects.filter(user=request.user).first()
-    return render(request, 'homepage.html', {
-        'colour_settings': colour_settings
-    })
+    if request.user.is_authenticated:
+        colour_settings = ColourSettings.objects.filter(user=request.user).first()
+        return render(request, 'homepage.html', {
+            'colour_settings': colour_settings
+        })
+    else:
+        return render(request, 'homepage.html')
 
 
 def area_list(request):
-    colour_settings = ColourSettings.objects.filter(user=request.user).first()
     all_area = Area.objects.all()
-    return render(request, 'apps/area/area_list.html', {
-        'colour_settings': colour_settings,
-        'all_area': all_area
-    })
+    if request.user.is_authenticated:
+        colour_settings = ColourSettings.objects.filter(user=request.user).first()
+        return render(request, 'apps/area/area_list.html', {
+            'colour_settings': colour_settings,
+            'all_area': all_area
+        })
+    else:
+        return render(request, 'apps/area/area_list.html', {
+            'all_area': all_area
+        })
 
 
 @login_required
@@ -69,23 +76,34 @@ def edit_area(request, area_id):
 
 
 def area_detail(request, area_id):
-    colour_settings = ColourSettings.objects.filter(user=request.user).first()
     area = Area.objects.get(id=area_id)
     available_candidate = Candidate.objects.filter(area=area)
-    return render(request, 'apps/area/area_detail.html', {
-        'colour_settings': colour_settings,
-        'area': area,
-        'available_candidate': available_candidate
-    })
+    if request.user.is_authenticated:
+        colour_settings = ColourSettings.objects.filter(user=request.user).first()
+        return render(request, 'apps/area/area_detail.html', {
+            'colour_settings': colour_settings,
+            'area': area,
+            'available_candidate': available_candidate
+        })
+    else:
+        return render(request, 'apps/area/area_detail.html', {
+            'area': area,
+            'available_candidate': available_candidate
+        })
 
 
 def candidate_list(request):
-    colour_settings = ColourSettings.objects.filter(user=request.user).first()
     all_candidate = Candidate.objects.all()
-    return render(request, 'apps/candidate/candidate_list.html', {
-        'colour_settings': colour_settings,
-        'all_candidate': all_candidate
-    })
+    if request.user.is_authenticated:
+        colour_settings = ColourSettings.objects.filter(user=request.user).first()
+        return render(request, 'apps/candidate/candidate_list.html', {
+            'colour_settings': colour_settings,
+            'all_candidate': all_candidate
+        })
+    else:
+        return render(request, 'apps/candidate/candidate_list.html', {
+            'all_candidate': all_candidate
+        })
 
 
 @login_required
@@ -133,9 +151,14 @@ def edit_candidate(request, candidate_id):
 
 
 def candidate_detail(request, candidate_id):
-    colour_settings = ColourSettings.objects.filter(user=request.user).first()
     candidate = Candidate.objects.get(id=candidate_id)
-    return render(request, 'apps/candidate/candidate_detail.html', {
-        'colour_settings': colour_settings,
-        'candidate': candidate
-    })
+    if request.user.is_authenticated:
+        colour_settings = ColourSettings.objects.filter(user=request.user).first()
+        return render(request, 'apps/candidate/candidate_detail.html', {
+            'colour_settings': colour_settings,
+            'candidate': candidate
+        })
+    else:
+        return render(request, 'apps/candidate/candidate_detail.html', {
+            'candidate': candidate
+        })
