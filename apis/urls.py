4a0b06c47e278260ed829cdb54467486d3f01dbd
Phawit Pornwattanakul <me@helloyeew.dev>
2022-10-06 11:34:00 +0700

    Add logout API path

--- apis/urls.py
+++ apis/urls.py
@@ -4,6 +4,7 @@ from apis.views import *
 
 urlpatterns = [
     path('login', LoginView.as_view(), name='api_login'),
+    path('logout', LogoutView.as_view(), name='api_logout'),
     path('profile', UserProfileView.as_view(), name='api_profile'),
     path('area', AreasView.as_view(), name='api_area_list'),
     path('candidate', CandidatesView.as_view(), name='api_candidate_list'),
