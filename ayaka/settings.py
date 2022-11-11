538ac458dc92a64c3eb600983d5f92e03efb79d2
Phawit Pornwattanakul <me@helloyeew.dev>
2022-11-11 22:56:03 +0700

    Add voter domain to whitelist

--- ayaka/settings.py
+++ ayaka/settings.py
@@ -186,6 +186,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://skdue-voter.helloyeew.dev/"
     # Voter's domain
 ]
 
