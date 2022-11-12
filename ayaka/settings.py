c93e579bc8845a5d72bc32c78c6997a6acb105bb
Phawit Pornwattanakul <me@helloyeew.dev>
2022-11-12 11:34:55 +0700

    Add trusted origin

--- ayaka/settings.py
+++ ayaka/settings.py
@@ -191,6 +191,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
     'https://skdue-voter.helloyeew.dev',
 ]
 
