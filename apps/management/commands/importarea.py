728fbe0c1aafcfff6148e9de809bfff30ed9b16e
Phawit Pornwattanakul <me@helloyeew.dev>
2022-11-25 05:16:43 +0700

    Change script URL to production

--- apps/management/commands/importarea.py
+++ apps/management/commands/importarea.py
@@ -12,34 +12,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # fetch data from government API
         # TODO: Change to production API
-        data = requests.get("http://localhost:8080/api/area").json()
+        data = requests.get("https://catnip-api.herokuapp.com/api/v1/locations").json()
+        print(data)
         self.stdout.write(self.style.SUCCESS('Found %s areas' % len(data)))
         # loop through data
         for area in data:
             try:
-                area_in_database = NewArea.objects.filter(area_id=area['location_id'])
+                area_in_database = NewArea.objects.filter(id=area['locationID']).first()
                 if area_in_database:
                     self.stdout.write(
-                        self.style.SUCCESS('Area "%s" already exists in database, updating...' % area['name']))
-                    area_in_database.update(
-                        id=area['location_id'],
-                        name=area['location'],
-                        population=area['population'],
-                        number_of_voters=area['number_of_voters'],
-                    )
+                        self.style.SUCCESS('Area "%s" already exists in database, updating...' % area['location']))
+                    area_in_database.id = area['locationID']
+                    area_in_database.name = area['location']
+                    area_in_database.population = area['population']
+                    area_in_database.number_of_voters = area['numberOfVoters']
                     area_in_database.save()
                 else:
-                    self.stdout.write(self.style.SUCCESS('Area with id "%s" not found in database, importing...' % area['location_id']))
+                    self.stdout.write(self.style.SUCCESS('Area with id "%s" not found in database, importing...' % area['locationID']))
                     NewArea.objects.create(
-                        id=area['location_id'],
+                        id=area['locationID'],
                         name=area['location'],
                         population=area['population'],
-                        number_of_voters=area['number_of_voters'],
+                        number_of_voters=area['numberOfVoters'],
                     )
                     self.stdout.write(
-                        self.style.SUCCESS(f"Area {area['location_id']} has been imported"))
+                        self.style.SUCCESS(f"Area {area['locationID']} has been imported"))
             except Exception as e:
-                self.stdout.write(self.style.ERROR(f"Cannot import area {area['location_id']}: {e}"))
+                self.stdout.write(self.style.ERROR(f"Cannot import area {area['locationID']}: {e}"))
                 # print full error to console
                 traceback.print_exc()
         self.stdout.write(self.style.SUCCESS('Import election area completed!'))
--- apps/management/commands/importpopulation.py
+++ apps/management/commands/importpopulation.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 
 from apps.models import NewArea
+from users.models import NewProfile
 
 
 class Command(BaseCommand):
@@ -13,49 +14,50 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # fetch data from government API
         # TODO: Change to production API
-        data = requests.get("http://localhost:8080/api/population").json()
+        data = requests.get("https://catnip-api.herokuapp.com/api/v1/populations").json()
         self.stdout.write(self.style.SUCCESS('Found %s population data' % len(data)))
         # loop through data
         for population in data:
             try:
                 # We use citizen ID as the username for the user so user can log in with their citizen ID
-                user_in_database = User.objects.get(username=population['citizen_id'])
+                user_in_database = User.objects.filter(username=str(population['citizenID'])).first()
                 if user_in_database:
                     self.stdout.write(
-                        self.style.SUCCESS('User "%s" already exists in database, updating...' % population['citizen_id']))
-                    user_in_database.update(
-                        username=population['citizen_id'],
-                        first_name=population['firstName'],
-                        last_name=population['lastName'],
-                    )
+                        self.style.SUCCESS('User "%s" already exists in database, updating...' % population['citizenID']))
+                    user_in_database.username = population['citizenID']
+                    user_in_database.first_name = population['firstName']
+                    user_in_database.last_name = population['lastName']
                     user_in_database.save()
-                    user_profile = user_in_database.newprofile
-                    user_profile.update(
-                        title=population['title'],
-                        area=NewArea.objects.get(area_id=population['locationID']),
-                        right_to_vote=population['rightToVote'],
-                        blacklist=population['blacklist']
-                    )
+                    try:
+                        user_profile = user_in_database.newprofile
+                    except:
+                        # create new profile
+                        user_profile = NewProfile.objects.create(user=user_in_database)
+                    user_profile.title = population['title']
+                    user_profile.sex = population['sex']
+                    user_profile.area = NewArea.objects.get(id=population['locationID'])
+                    user_profile.right_to_vote = population['rightToVote']
+                    user_profile.blacklist = population['blacklist']
                     user_profile.save()
                 else:
-                    self.stdout.write(self.style.SUCCESS('User with id "%s" not found in database, importing...' % population['citizen_id']))
+                    self.stdout.write(self.style.SUCCESS('User with id "%s" not found in database, importing...' % population['citizenID']))
                     user = User.objects.create(
-                        username=population['citizen_id'],
+                        username=population['citizenID'],
                         first_name=population['firstName'],
                         last_name=population['lastName'],
                     )
                     user.save()
-                    user.newprofile.update(
-                        title=population['title'],
-                        area=NewArea.objects.get(area_id=population['locationID']),
-                        right_to_vote=population['rightToVote'],
-                        blacklist=population['blacklist']
-                    )
-                    user.newprofile.save()
+                    profile = user.newprofile
+                    profile.title = population['title']
+                    profile.sex = population['sex']
+                    profile.area = NewArea.objects.get(id=population['locationID'])
+                    profile.right_to_vote = population['rightToVote']
+                    profile.blacklist = population['blacklist']
+                    profile.save()
                     self.stdout.write(
-                        self.style.SUCCESS(f"User {population['citizen_id']} has been imported"))
+                        self.style.SUCCESS(f"User {population['citizenID']} has been imported"))
             except Exception as e:
-                self.stdout.write(self.style.ERROR(f"Cannot import user {population['citizen_id']}: {e}"))
+                self.stdout.write(self.style.ERROR(f"Cannot import user {population['citizenID']}: {e}"))
                 # print full error to console
                 traceback.print_exc()
         self.stdout.write(self.style.SUCCESS('Import population completed!'))
